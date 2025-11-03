84e671e13861c05fdc6ecac3b3d357fed2a89651
Anbarasu2410 <anbuarasu2017@gmail.com>
2025-11-03 15:03:51 +0530

    url changes reverted

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://smt-erp-driver-app-api.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
